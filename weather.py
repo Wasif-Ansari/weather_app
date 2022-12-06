5bd885f1fc21585510bdb03d6c0e3c911c19fff2
Wasif Ansari <wasifansari788815@gmail.com>
2022-12-06 17:07:48 +0530

    creating label for name

--- weather.py
+++ weather.py
@@ -2,9 +2,16 @@ from tkinter import *
 from tkinter import ttk #combobox
 import requests #to use apis
 
+
 win = Tk()
 win.title("Weather")
 win.config(bg="blue")
 win.geometry("500x600")
+
+city_name = StringVar()
+
+name = Label(win,text="MY WEATHER APP",font=("Time New Roman",30,"bold"))
+name.place(x=25, y=50,height=50,width=450)
+
 
 win.mainloop()
