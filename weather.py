0a252bfbc01c99472332e2e1661768fa846f7f5f
Wasif Ansari <wasifansari788815@gmail.com>
2022-12-06 17:03:38 +0530

    importing tkinter and creating window

---
+++ weather.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk #combobox
+import requests #to use apis
+
+win = Tk()
+win.title("Weather")
+win.config(bg="blue")
+win.geometry("500x600")
+
+win.mainloop()
